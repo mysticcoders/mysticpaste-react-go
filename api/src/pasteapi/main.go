efb3fc4aba3c6d7e29508200c84c542c7a66604c
Andrew Lombardi <andrew@mysticcoders.com>
2017-05-10 20:23:12 -0700

    classification is still messed up

--- api/src/pasteapi/main.go
+++ api/src/pasteapi/main.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/spf13/viper"
 	multibayes "github.com/lytics/multibayes"
+	"regexp"
 )
 
 // DB object for use throughout project
@@ -57,35 +58,33 @@ func main() {
 	DB.Where("abuse = ?", true).Find(&abusePastes)
 	DB.Where("abuse = ?", false).Find(&goodPastes)
 
-
 	classifier := multibayes.NewClassifier()
 	classifier.MinClassSize = 0
 
-	for _, paste := range abusePastes {
-		classifier.Add(paste.Content, []string{"Bad"})
-	}
-	for _, paste := range goodPastes {
-		classifier.Add(paste.Content, []string{"Good"})
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	for _, goodPaste := range goodPastes {
+		processedString := reg.ReplaceAllString(goodPaste.Content, " ")
+		log.Print(processedString)
+		classifier.Add(processedString, []string{"Good"})
 	}
 
-	//classifier.Add("good message", []string{"Good"})
-	//classifier.Add("bad message", []string{"Bad"})
-	//probs := classifier.Posterior("good message")
-	//fmt.Printf("Posterior Probabilities: %+v\n", probs)
-	//
-	//probs = classifier.Posterior("bad message")
-	//fmt.Printf("Posterior Probabilities: %+v\n", probs)
+	for _, badPaste := range abusePastes {
+		processedString := reg.ReplaceAllString(badPaste.Content, " ")
+		//log.Print(processedString)
+		classifier.Add(processedString, []string{"Bad"})
+	}
+
+	good_text := "Fetch feed for each group since a given time but only get 1 feed item"
+	good_probs := classifier.Posterior(good_text)
+	fmt.Printf("Posterior Probabilities for Good: %+v\n", good_probs)
+
 
 	//bad_text := "A proxy-server is just a host that works to a different being an intermediary in one community. Whenever a client from another host is requesting a document, the proxy functions like a filter for that document subsequently examines the document based on the protection filter before providing the required document towards the customer of the primary server. About the hand you may still find unblocked games in college and these generally include other press activities, along with motion, journey, game, problem, firing, activities, technique, that are academic and all enjoyable. They may be performed on unblockgamesatschool.com. Additionally they concentrate on the gamers' abilities. Be advised although that proxy-server differs to a different in one college."
-	bad_text := "Buy Hacked PayPal, Bank wire logins, Wu transfer, cc top-up, cvv, smtp, rdp, inbox mailer, email leads, dumps, warez, with proofs of transactions and accounts. Hello There, I'm a Russian American certified carder and hacker with years of experience. i am selling hacked Wu transfer, Hacked PayPal accounts, Bank wire logins, Money Booker, skrill account, cc top-up all transactions are secured and with zero theft and no charge back."
+	//bad_text := "Buy Hacked PayPal Bank wire logins Wu transfer cc topup cvv smtp rdp inbox mailer email leads dumps warez with proofs of transactions and accounts Hello There Im a Russian American certified carder and hacker with years of experience i am selling hacked Wu transfer Hacked PayPal accounts Bank wire logins Money Booker skrill account cc topup all transactions are secured and with zero theft and no charge back"
+	bad_text := "Beaute Lift Anti Aging Serum I m for natural skin care But don t take my word because Try some out yourself read customer reviews and produce a decision after the idea The best companies will have a no risk guarantee if when you are around 100 satisfied you can send full or used bottles back and get the actual full refund Trial http yoursantiagingserum com beaute lift anti aging serum"
 
 	probs := classifier.Posterior(bad_text)
-	fmt.Printf("Posterior Probabilities: %+v\n", probs)
-
-	good_text := "Fetch feed for each group, since a given time, but only get 1 feed item"
-	good_probs := classifier.Posterior(bad_text)
-	fmt.Printf("Posterior Probabilities: %+v\n", good_probs)
-
+	fmt.Printf("Posterior Probabilities for Bad: %+v\n", probs)
 
 
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", ApiTokenHeaderKey})
