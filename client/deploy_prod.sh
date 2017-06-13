8cf77e3c23b7843a78944d989bd64c6848ac7b2b
Andrew Lombardi <andrew@mysticcoders.com>
2017-06-13 15:42:23 -0700

    continue to work on the circle deployment

--- client/deploy_prod.sh
+++ client/deploy_prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-scp $CIRCLE_ARTIFACTS/client.tgz deploy@50.116.29.145:~
+# TODO figure out why it is timing out
+# scp $CIRCLE_ARTIFACTS/client.tgz deploy@50.116.29.145:~
 # ssh deploy@173.255.217.67 bash -c "'
 #   cd /var/www/vhosts/scholarbee.net/httpdocs/spelling &&
 #   tar czvf spelling.tgz * &&
