5d7ae7afe135925c0364659bf46afaba1a55cfda
Andrew Lombardi <andrew@mysticcoders.com>
2017-06-13 23:50:21 -0700

    finish off working deploy script

--- api/src/deploy_prod.sh
+++ api/src/deploy_prod.sh
@@ -6,5 +6,8 @@ scp $CIRCLE_ARTIFACTS/pasteapi.tgz deploy@50.116.29.145:~
 ssh deploy@50.116.29.145 bash -c "'
   mv pasteapi.tgz /var/www/vhosts/mysticpaste.com/api &&
   cd /var/www/vhosts/mysticpaste.com/api &&
+  sudo service pasteapi stop
   tar xzvf pasteapi.tgz
+  sudo service pasteapi start
+  rm -rf pasteapi.tgz
 '"
