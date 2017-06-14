62f534a875e763eb8abe2211a44affd6a0a609c1
Andrew Lombardi <andrew@mysticcoders.com>
2017-06-13 22:16:37 -0700

    fix up the deploy prod for at least moving files

--- client/deploy_prod.sh
+++ client/deploy_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # TODO figure out why it is timing out
-# scp $CIRCLE_ARTIFACTS/client.tgz deploy@50.116.29.145:~
+scp $CIRCLE_ARTIFACTS/client.tgz deploy@50.116.29.145:~
 # ssh deploy@173.255.217.67 bash -c "'
 #   cd /var/www/vhosts/scholarbee.net/httpdocs/spelling &&
 #   tar czvf spelling.tgz * &&
