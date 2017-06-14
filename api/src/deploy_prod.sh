426d5d6a6998a806c54a1171bec5546a1dad129c
Andrew Lombardi <andrew@mysticcoders.com>
2017-06-13 19:28:05 -0700

    add a deploy_prod for server and see if we can get it functioning

---
+++ api/src/deploy_prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO figure out why it is timing out
+scp $CIRCLE_ARTIFACTS/pasteapi deploy@50.116.29.145:~
+# ssh deploy@173.255.217.67 bash -c "'
+#   cd /var/www/vhosts/scholarbee.net/httpdocs/spelling &&
+#   tar czvf spelling.tgz * &&
+#   mv spelling.tgz ../../archive &&
+#   rm -rf * &&
+#   mv ~/client.tgz /var/www/vhosts/scholarbee.net/httpdocs/spelling &&
+#   tar xzvf client.tgz
+# '"
